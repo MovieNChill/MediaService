8de0c5048260f7d317a1ede7b5d8b37e4b36302c
Yoann Robinet <yoann@yopbox.com>
2023-01-15 19:18:27 +0100

    temporary change route

--- src/main/java/com/movienchill/mediaservice/constants/Router.java
+++ src/main/java/com/movienchill/mediaservice/constants/Router.java
@@ -2,12 +2,12 @@ package com.movienchill.mediaservice.constants;
 
 public class Router {
     /* BASE API */
-    public static final String API_START = "/api";
+    public static final String API_START = "";//"/api";
 
     /* BASE URL */
     public static final String BASE_AVIS = API_START + "/avis";
     public static final String BASE_GENRE = API_START + "/genres";
-    public static final String BASE_MEDIA = API_START + "/medias";
+    public static final String BASE_MEDIA = API_START + "/";
     public static final String BASE_NOTE = API_START + "/notes";
     public static final String BASE_PLATFORM = API_START + "/plateforms";
 
